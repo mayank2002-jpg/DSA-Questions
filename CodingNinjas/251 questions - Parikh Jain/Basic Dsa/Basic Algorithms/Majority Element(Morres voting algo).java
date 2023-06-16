efa808bb2ab5c6096690e84e86728a6673e6b81f
Mayank <mayankmahajan936@gmail.com>
2023-06-16 16:13:32 +0530

    sorting

---
+++ CodingNinjas/251 questions - Parikh Jain/Basic Dsa/Basic Algorithms/Majority Element(Morres voting algo).java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.* ;
+
+public class Solution {
+	public static int findMajority(int[] arr, int n) {
+		int ans = arr[0];
+		int count = 1;
+
+		for(int i = 0;i<n;i++){
+			if(arr[i] == ans){
+				count++;
+			}
+			else{
+				count--;
+			}
+			if(count==0){
+				ans = arr[i];
+				count = 1;
+			}
+		}
+		count = 0;
+		for(int i = 0;i<n;i++){
+			if(arr[i]==ans){
+				count++;
+			}
+		}
+		return(count>n/2)?ans:-1;
+	}
+}
