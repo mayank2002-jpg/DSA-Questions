c868c15b6ca93f7945cdab44cb0c7757ebf932fb
Mayank <mayankmahajan936@gmail.com>
2023-06-27 11:14:42 +0530

    2D-Array

---
+++ Leetcode/2D-Arrays/867. Transpose Matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int m = A.length, n = A[0].length;
+        int [][] B = new int[n][m];
+        for(int i = 0;i<m;i++){
+            for(int j=0;j<n;j++){
+                B[j][i] = A[i][j];
+            }
+        }
+        return B;
+    }
+}
