a86f82229d1b72321bc0b973b1370a4807ed8d97
Mayank <mayankmahajan936@gmail.com>
2023-07-24 16:34:30 +0530

    Top 50 Arrays Question

---
+++ GFG/Top 50 Arrays Question/3. Reverse a String.java
@@ -0,0 +1,13 @@
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        String rev = "";
+        for(int i = str.length()-1; i>=0; i--){
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
+}
