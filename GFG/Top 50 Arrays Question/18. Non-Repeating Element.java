e53f002000a72709a2721adc56fcb853dcb5fcfb
Mayank <mayankmahajan936@gmail.com>
2023-07-28 00:18:42 +0530

    Top 50 Arrays Question

---
+++ GFG/Top 50 Arrays Question/18. Non-Repeating Element.java
@@ -0,0 +1,20 @@
+class Check{
+    
+    public int firstNonRepeating(int arr[], int n) 
+    { 
+        Map<Integer, Integer> m = new HashMap<>(); 
+        for (int i = 0; i < n; i++) { 
+            if (m.containsKey(arr[i])) { 
+                m.put(arr[i], m.get(arr[i]) + 1); 
+            } 
+            else { 
+                m.put(arr[i], 1); 
+            } 
+        } 
+        for (int i = 0; i < n; i++) 
+            if (m.get(arr[i]) == 1) 
+                return arr[i]; 
+        return 0; 
+    }  
+    
+} 
