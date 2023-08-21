8cd008834dfd2a7dc19c403a469ecc8b87fba595
Mayank <mayankmahajan936@gmail.com>
2023-08-21 13:21:36 +0530

    Arrays

---
+++ CodingNinjas/Arrays/Easy/Largest Element in the Array.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            if(max<arr[i]){
+                max = arr[i];
+            }
+           
+        }
+        return max;
+    }
+}
