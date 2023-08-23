ae1ef1cf05d99ee409457e7329de08a6fccb360d
Mayank <mayankmahajan936@gmail.com>
2023-08-23 18:21:47 +0530

    Arrays

---
+++ CodingNinjas/Arrays/Easy/Rotate array by k Position.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+import java.io.*;
+
+class Solution {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            arr[i] = sc.nextInt();
+
+        }
+
+        int K = sc.nextInt();
+
+        for (int j = 0; j < K; j++) {
+
+            for (int k = n - 1; k >= 0; k--) {
+
+                int temp = arr[0];
+
+                arr[0] = arr[k];
+
+                arr[k] = temp;
+
+            }
+
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print(arr[i] + " ");
+
+        }
+
+    }
+
+}
