81ebd6db2adcbbf17c8930a92aa310ffbbe4418b
Mayank <mayankmahajan936@gmail.com>
2023-09-03 14:45:45 +0530

    Binary search

---
+++ CodingNinjas/Binary Search/Implement upper bound.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static int upperBound(int []arr, int x, int n){
+        int low = 0, high = n-1, ans = n;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]>x){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
