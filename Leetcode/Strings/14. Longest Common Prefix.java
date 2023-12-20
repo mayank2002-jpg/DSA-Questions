321d4e71f46be6088d2215949c189bc3427354ea
Mayank <mayankmahajan936@gmail.com>
2023-12-20 12:05:41 +0530

    Strings

--- Leetcode/Strings/14. Longest Common Prefix.java
+++ Leetcode/Strings/14. Longest Common Prefix.java
@@ -1,17 +1,18 @@
 class Solution {
-public String longestCommonPrefix(String[] strs) {
-    Arrays.sort(strs);
-    String s1 = strs[0];
-    String s2 = strs[strs.length-1];
-    int i = 0;
-    while(i<s1.length() && i<s2.length()){
-        if(s1.charAt(i)==s2.charAt(i)){
-            i++;
-        }
-        else{
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+
+        while(idx<s1.length() && idx<s2.length()){
+            if(s1.charAt(idx)==s2.charAt(idx)){
+                idx++;
+            }
+            else{
             break;
+            }
         }
+        return s1.substring(0,idx);
     }
-    return s1.substring(0,i);
-}
 }
